8c565e04b0598ade8f2dd9d2d680e8e4e54ff6f4
yash.srivastava <yash.srivastava@naukri.com>
2025-03-14 21:57:48 +0530

    requirements and port update

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-<<<<<<< HEAD
-import dotenv from "dotenv";
-dotenv.config();
-=======
 
->>>>>>> cb
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -51,13 +46,6 @@ app.use((req, res, next) => {
     res.status(status).json({ message });
     throw err;
   });
-<<<<<<< HEAD
-=======
-
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
->>>>>>> cb
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
