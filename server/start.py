82236e868779c64e145e3ae74f8c97cd8e619655
yash.srivastava <yash.srivastava@naukri.com>
2025-03-19 17:55:27 +0530

    update

--- server/start.py
+++ server/start.py
@@ -2,5 +2,5 @@ import uvicorn
 from chatbot.api import app
 import os
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
