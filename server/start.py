987b5812ddf9b7944284f2a028a6d1a9cd3f1d4c
yash.srivastava <yash.srivastava@naukri.com>
2025-03-21 14:02:46 +0530

    port fix

--- server/start.py
+++ server/start.py
@@ -3,4 +3,4 @@ from chatbot.api import app
 import os
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
