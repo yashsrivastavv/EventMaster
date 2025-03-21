ca99bd3b3df265c24d4f2b5bbacbbb999550d7cf
yash.srivastava <yash.srivastava@naukri.com>
2025-03-21 13:54:35 +0530

    port fix

--- server/index.ts
+++ server/index.ts
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
   } else {
     serveStatic(app);
   }
-  const port = process.env.PORT || 5000
+  const port =  5000
   server.listen({
     port,
     host: "0.0.0.0",
